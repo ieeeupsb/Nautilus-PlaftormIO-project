f949a8f0a4fd3298520a6c704b8d8e19a35ef1c1
viniciuspirassoli <up20190024@g.uporto.pt>
2023-03-23 17:13:39 +0000

    got encoder to work in a class

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "Adafruit_MotorShield.h"
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 #include <SPI.h>
+#include "encoder.h"
 
 #define ENCA_LEFT 23
 #define ENCB_LEFT 5
@@ -12,8 +13,7 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *motor_left = AFMS.getMotor(1);
 Adafruit_DCMotor *motor_right = AFMS.getMotor(2);
 
-volatile int leftEncCount = 0;
-volatile int rightEncCount = 0;
+Encoder leftEncoder(ENCA_LEFT, ENCB_LEFT);
 
 const int ir1 = 39;
 const int ir2 = 36;
@@ -22,13 +22,10 @@ const int ir4 = 35;
 const int ir5 = 32;
 const int iman = 19;
 
-void setupEncoders();
-void leftISR();
-void rightISR();
-
 void setup()
 {
-  setupEncoders();
+  //setupEncoders();
+  leftEncoder.setup();
   AFMS.begin();
   Serial.begin(115200);
 
@@ -43,45 +40,11 @@ void setup()
 void loop()
 {
 
+  Serial.print("LEFT ENC: ");
+  Serial.println(leftEncoder.getCount());
   digitalWrite(iman, LOW);
-
-  Serial.print("Left Encoder Count: ");
-  Serial.print(leftEncCount);
-  Serial.print(" | Right Encoder Count: ");
-  Serial.println(rightEncCount);
 
   delay(20);
 }
 
 // -------- //
-
-//Encoder Interrupt Service Routines (ISRs)
-//IRAM_ATTR makes sure that the code is stored in the internal RAM of the ESP32,
-//which is way faster than the flash memory that the code usually resides in.
-
-void IRAM_ATTR leftISR() {
-  if (digitalRead(ENCB_LEFT)) {
-    leftEncCount--;
-  }
-  else {
-    leftEncCount++;
-  }
-}
-
-void IRAM_ATTR rightISR() {
-  if (digitalRead(ENCB_RIGHT)) {
-    leftEncCount--;
-  }
-  else {
-    leftEncCount++;
-  }
-}
-
-void setupEncoders() {
-  pinMode(ENCA_LEFT, INPUT);
-  pinMode(ENCB_LEFT, INPUT);
-  pinMode(ENCA_RIGHT, INPUT);
-  pinMode(ENCB_RIGHT, INPUT);
-  attachInterrupt(digitalPinToInterrupt(ENCA_LEFT), leftISR, RISING);
-  attachInterrupt(digitalPinToInterrupt(ENCA_RIGHT), rightISR, RISING);
-}
