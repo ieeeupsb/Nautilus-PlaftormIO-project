bdea17ab804cf5cd2b724baeb3a7e05af997c1bd
viniciuspirassoli <up20190024@g.uporto.pt>
2023-03-24 14:09:24 +0000

    added a few things to motorcontroller

--- lib/MotorController/MotorController.h
+++ lib/MotorController/MotorController.h
@@ -2,15 +2,18 @@
 #include <Arduino.h>
 #include "PID_v1.h"
 #include "encoder.h"
+#include <Adafruit_MotorShield.h>
 
 class MotorController {
     public:
-        MotorController();
-        ~MotorController();
+        MotorController(); // Constructor - Needs parameters
+        ~MotorController(); // Destructor
         //What does the controller need to be able to do?
     private:
         //What data do we need to keep track of?
 
+        // Object pointers:
         PID *leftPID, *rightPID;
         Encoder *leftEncoder, *rightEncoder;
+        Adafruit_MotorShield *motorShield;
 };
